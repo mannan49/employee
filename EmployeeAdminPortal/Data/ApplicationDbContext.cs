f28e206855a96a0614f66ae3ad1658e7e85adf18
manan.nasir <manan.nasir@CMDLHRLTX682>
2024-09-05 08:03:34 -0400

    Add project files.

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EmployeeAdminPortal.Models.Entities;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+ 
